28d1156b745b24b8d9a99c7f1b6907b098d6bc85
raivox <ouchkaihsmail@gmail.com>
2024-01-21 20:18:31 +0100

    added multip caculation

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ program
   .command('sum <arg1> <arg2>')
   .description('Calculate the sum of two numbers')
   .action((arg1, arg2) => {
-    // Your command logic here
+
     const number1 = parseFloat(arg1);
     const number2 = parseFloat(arg2);
 
@@ -30,14 +30,28 @@ program
     if(isNaN(number1) || isNaN(number2)){
         console.error('Error: Please provide valid numbers.');
     }else{
-
-
-        
-        
         // calculation and print the sum 
         const sum = number1 + number2;
         console.log(`Sum ${number1} + ${number2} is: ${sum}`); 
     }
   });
+
+
+program
+  .command('mul <arg1> <arg2>')
+  .description('calculation multiplay two numbers')
+  .action((num1, num2)=>{
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+
+    if(isNaN(number1) || isNaN(num2)){
+      console.error('Error: Please provide valid numbers.');
+    }else{
+      const mul = number1 * number2;
+      console.log(`Mul ${number1} * ${number2} is: ${mul}`); 
+    }
+  })
+
+
 
 program.parse(process.argv);
